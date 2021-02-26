86513dd7c55c965e3b179840e6b26d85f1769097
john <john.d.leonetti@gmail.com>
2021-02-25 20:37:50 -0800

    commit tree 4e57b0e91684c5f9808e3d1931f3c192f4718546

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require "pry"
+def prime?(number)
+    if number <= 1
+        return false
+    elsif (2...number).any? {|f_ing_prime| number % f_ing_prime == 0 }
+      
+       return false
+else
+        return true
+           #end # ends do
+    end # ends if  
+end # ends method
+
